e6e38aded556113c5caeb0107bb4599db56bc9bf
Jorge <jorgelizgo@gmail.com>
2024-03-07 19:43:24 +0100

    entities

--- terrarium/env/engine.py
+++ terrarium/env/engine.py
@@ -9,13 +9,45 @@ clock = pygame.time.Clock()
 running = True
 friction = 0.1
 
-class Agent():
 
-    def __init__(self, initial_pos, radius,velocity,direction,vision_len):
+class Entity():
+    
+    def draw():
+        pass 
+
+class Obstacle(Entity):
+
+    def __init__(self, initial_pos, size, color):
         self.pos = initial_pos
+        self.color = color
+        self.size = size
+        self.collision_rect = pygame.Rect(self.pos[0]-self.size[0]/2,self.pos[1]-self.size[1]/2,self.size[0],self.size[1])
+
+    def draw(self):
+        pygame.draw.rect(screen,self.color,self.collision_rect,1)
+    
+
+class Food(Entity):
+
+    def __init__(self, initial_pos, radius, color):
+        self.pos = initial_pos
+        self.radius = radius
+        self.color = color
+        self.collision_rect = pygame.Rect(self.pos[0]-self.radius,self.pos[1]-self.radius,self.radius*2,self.radius*2)
+
+    def draw(self):
+        #pygame.draw.rect(screen,"black",self.collision_rect,1)
+        pygame.draw.circle(screen, self.color, self.pos, self.radius,0)
+
+class Agent(Entity):
+
+    def __init__(self, initial_pos, radius,velocity,direction,vision_len,color):
+        self.pos = initial_pos
+        self.radius = radius
+        self.color = color
+        self.collision_rect = pygame.Rect(self.pos[0]-self.radius,self.pos[1]-self.radius,self.radius*2,self.radius*2)
         self.velocity = velocity
         self.direction = np.deg2rad(direction)
-        self.radius = radius
         self.vision_len = vision_len
         self.vision = np.zeros((11,2))
         self.vision_color = np.empty((11), dtype=object)
@@ -25,15 +57,13 @@ class Agent():
             self.vision[idx][1] = self.pos[1] - self.vision_len*math.sin(self.direction+np.deg2rad((idx-5)*10))
             self.vision_color[idx] = "black"
             self.collision_distance[idx] = 999
-        self.agent_color = "black"
-        self.collision_rect = pygame.Rect(self.pos[0]-self.radius,self.pos[1]-self.radius,self.radius*2,self.radius*2)
         self.dv = np.zeros(2)
 
     def draw(self):
         #pygame.draw.rect(screen,"black",self.collision_rect,1)
         for idx,vision in enumerate(self.vision):
             pygame.draw.aaline(screen, self.vision_color[idx], self.pos,vision)
-        pygame.draw.circle(screen, self.agent_color, self.pos, self.radius,0)
+        pygame.draw.circle(screen, self.color, self.pos, self.radius,0)
 
     def update(self):
         self.pos += self.dv
@@ -78,7 +108,8 @@ class Agent():
 
 if __name__=="__main__":
 
-    adan = Agent(np.array([screen.get_width() / 2, screen.get_height() / 2]),40,4,90,200)
+    adan = Agent(np.array([screen.get_width() / 2, screen.get_height() / 2]),40,4,90,200,"black")
+    elements = [Obstacle(np.array([600,400]), np.array([200,100]), "black"),Food(np.array([400,400]),30,"green")]
 
     while running:
         # fill the screen with a color to wipe away anything from last frame
@@ -90,30 +121,40 @@ if __name__=="__main__":
                 running = False
 
 
-        obstacle = pygame.draw.rect(screen, "black",pygame.Rect(200, 100, 200, 100),1)
-
-        #MOVEMENT ACTIONS
         keys = pygame.key.get_pressed()
-        block_collide = obstacle.colliderect(adan.collision_rect)
-        if not block_collide:
+
+        collisions = [idx for idx,elem in enumerate(elements) if elem.collision_rect.colliderect(adan.collision_rect)]
+        if not collisions:
             adan.move(keys)
         else:
-            adan.dv = -adan.dv
+            bounce = False
+            for c in collisions:
+                if type(elements[c]) is Food:
+                    elements.pop(c)
+                else:
+                    bounce = True
+            if bounce:
+                adan.dv = -adan.dv
 
         adan.update()
 
         for idx,vision in enumerate(adan.vision):
-            line_collide = obstacle.clipline(adan.pos,vision)
-            if line_collide:
-                adan.vision_color[idx]="red"
-                adan.vision[idx]=np.array(line_collide[0])
-                adan.collision_distance[idx] = np.sqrt((adan.pos[0]-vision[0])**2+(adan.pos[1]-vision[1])**2)-adan.radius
-            else:
-                adan.vision_color[idx]="black"
+            for elem in elements:
+                line_collide = elem.collision_rect.clipline(adan.pos,vision)
+                if line_collide:
+                    adan.vision_color[idx]="red"
+                    adan.vision[idx]=np.array(line_collide[0])
+                    adan.collision_distance[idx] = np.sqrt((adan.pos[0]-vision[0])**2+(adan.pos[1]-vision[1])**2)-adan.radius
+                    break
+                else:
+                    adan.vision_color[idx]="black"
+                    adan.collision_distance[idx]=999
 
         # RENDER YOUR GAME HERE
-        adan.draw()
 
+        for elem in elements:
+            elem.draw()
+        adan.draw()
         # flip() the display to put your work on screen
         pygame.display.flip()
 

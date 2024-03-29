45fc46fc342d64f89bbf93c2e69fbeaafd64d00c
Jorge <jorgelizgo@gmail.com>
2024-03-29 10:43:10 +0100

    start training

--- train.py
+++ train.py
@@ -1,114 +1,95 @@
-
-"""Uses Stable-Baselines3 to train agents to play the Waterworld environment using SuperSuit vector envs.
-
-For more information, see https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html
-
-Author: Elliot (https://github.com/elliottower)
-"""
-from __future__ import annotations
-
-import glob
 import os
-import time
 
+import ray
 import supersuit as ss
-from stable_baselines3 import PPO
-from stable_baselines3.ppo import MlpPolicy
+from ray import tune
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.tune.registry import register_env
+from torch import nn
 
 import terrarium.terrarium_v0 as Terrarium
 
-#from pettingzoo.sisl import waterworld_v4
-
-
-def train(
-    env_fn,env_kwargs, steps: int = 10_000, seed: int | None = 0,
-):
-    # Train a single model to play as each agent in a cooperative Parallel environment
-    env = env_fn.env(env_kwargs)
-
-    env.reset(seed=seed)
-
-    print(f"Starting training on {str(env.metadata['name'])}.")
-
-    #env = ss.pettingzoo_env_to_vec_env_v1(env)
-    #env = ss.concat_vec_envs_v1(env, 8, num_cpus=2, base_class="stable_baselines3")
-
-    # Note: Waterworld's observation space is discrete (242,) so we use an MLP policy rather than CNN
-    model = PPO(
-        MlpPolicy,
-        env,
-        verbose=3,
-        learning_rate=1e-3,
-        batch_size=256,
-    )
-
-    model.learn(total_timesteps=steps)
-
-    model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
-
-    print("Model has been saved.")
-
-    print(f"Finished training on {str(env.unwrapped.metadata['name'])}.")
-
-    env.close()
-
-
-def eval(env_fn, num_games: int = 100, render_mode: str | None = None, **env_kwargs):
-    # Evaluate a trained agent vs a random agent
-    env = env_fn.env(render_mode=render_mode, **env_kwargs)
-    print(
-        f"\nStarting evaluation on {str(env.metadata['name'])} (num_games={num_games}, render_mode={render_mode})"
-    )
-
-    try:
-        latest_policy = max(
-            glob.glob(f"{env.metadata['name']}*.zip"), key=os.path.getctime
+class CNNModelV2(TorchModelV2, nn.Module):
+    def __init__(self, obs_space, act_space, num_outputs, *args, **kwargs):
+        TorchModelV2.__init__(self, obs_space, act_space, num_outputs, *args, **kwargs)
+        nn.Module.__init__(self)
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, [8, 8], stride=(4, 4)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, [4, 4], stride=(2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, [3, 3], stride=(1, 1)),
+            nn.ReLU(),
+            nn.Flatten(),
+            (nn.Linear(3136, 512)),
+            nn.ReLU(),
         )
-    except ValueError:
-        print("Policy not found.")
-        exit(0)
+        self.policy_fn = nn.Linear(512, num_outputs)
+        self.value_fn = nn.Linear(512, 1)
 
-    model = PPO.load(latest_policy)
+    def forward(self, input_dict, state, seq_lens):
+        model_out = self.model(input_dict["obs"].permute(0, 3, 1, 2))
+        self._value_out = self.value_fn(model_out)
+        return self.policy_fn(model_out), state
 
-    rewards = {agent: 0 for agent in env.possible_agents}
+    def value_function(self):
+        return self._value_out.flatten()
+    
 
-    # Note: We train using the Parallel API but evaluate using the AEC API
-    # SB3 models are designed for single-agent settings, we get around this by using he same model for every agent
-    for i in range(num_games):
-        env.reset(seed=i)
+def env_creator(settings):
 
-        for agent in env.agent_iter():
-            obs, reward, termination, truncation, info = env.last()
-
-            for a in env.agents:
-                rewards[a] += env.rewards[a]
-            if termination or truncation:
-                break
-            else:
-                act = model.predict(obs, deterministic=True)[0]
-
-            env.step(act)
-    env.close()
-
-    avg_reward = sum(rewards.values()) / len(rewards.values())
-    print("Rewards: ", rewards)
-    print(f"Avg reward: {avg_reward}")
-    return avg_reward
+    env = Terrarium.env(settings)
+    env = ss.color_reduction_v0(env, mode="B")
+    env = ss.dtype_v0(env, "float32")
+    env = ss.resize_v1(env, x_size=84, y_size=84)
+    env = ss.normalize_obs_v0(env, env_min=0, env_max=1)
+    env = ss.frame_stack_v1(env, 3)
+    return env
 
 
 if __name__ == "__main__":
-    env_fn = Terrarium
-    env_kwargs = {
-        "agents":3,
-        "obstacles":1,
+    ray.init()
+
+    env_name = "terrarium"
+    
+    settings = {
+        "agents":5,
+        "obstacles":3,
         "food":5
     }
+    register_env(env_name, lambda config: ParallelPettingZooEnv(env_creator(settings)))
+    ModelCatalog.register_custom_model("CNNModelV2", CNNModelV2)
 
-    # Train a model (takes ~3 minutes on GPU)
-    train(env_fn,env_kwargs, steps=196_608, seed=0)
+    config = (
+        PPOConfig()
+        .environment(env=env_name, clip_actions=True)
+        .rollouts(num_rollout_workers=4, rollout_fragment_length=128)
+        .training(
+            train_batch_size=512,
+            lr=2e-5,
+            gamma=0.99,
+            lambda_=0.9,
+            use_gae=True,
+            clip_param=0.4,
+            grad_clip=None,
+            entropy_coeff=0.1,
+            vf_loss_coeff=0.25,
+            sgd_minibatch_size=64,
+            num_sgd_iter=10,
+        )
+        .debugging(log_level="ERROR")
+        .framework(framework="torch")
+        .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
+    )
 
-    # Evaluate 10 games (average reward should be positive but can vary significantly)
-    #eval(env_fn, num_games=10, render_mode=None, **env_kwargs)
-
-    # Watch 2 games
-    #eval(env_fn, num_games=2, render_mode="human", **env_kwargs)
+    tune.run(
+        "PPO",
+        name="PPO",
+        stop={"timesteps_total": 5000000 if not os.environ.get("CI") else 50000},
+        checkpoint_freq=10,
+        local_dir="~/ray_results/" + env_name,
+        config=config.to_dict(),
+    )

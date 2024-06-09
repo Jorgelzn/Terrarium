9fc4ada19c39f5c1e3f75e09994b7a579248769e
Jorge <jorgelizgo@gmail.com>
2024-06-09 18:57:54 +0200

    allow smaller map than screen

--- tests/test_env.py
+++ tests/test_env.py
@@ -3,7 +3,7 @@ from terrarium import terrarium_v0
 from pettingzoo.test import parallel_api_test
 
 if __name__ == "__main__":
-    env = terrarium_v0.parallel_env(render_mode="human",voxels=5)
+    env = terrarium_v0.parallel_env(render_mode="human",voxels=10)
     #parallel_api_test(env, num_cycles=1_000_000)
 
     observations, infos = env.reset()
